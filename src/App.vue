837225fdea885f6691617341ca80e7e9b90c1423
biyanwen <biyw@tsintergy.com>
2021-08-07 15:22:37 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Button>button</Button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {defineComponent} from 'vue';
+import {Button} from 'hand-painted-ui-vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 });
 </script>
